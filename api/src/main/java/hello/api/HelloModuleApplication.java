88546fc26ad44797ad48fb256bac22ccde8b1500
Jeong Myeonggu <audrn6689@gmail.com>
2023-08-26 04:13:20 +0900

    step - 1

--- api/src/main/java/hello/api/HelloModuleApplication.java
+++ api/src/main/java/hello/api/HelloModuleApplication.java
@@ -3,7 +3,6 @@ package hello.api;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
-
 @SpringBootApplication
 public class HelloModuleApplication {
     public static void main(String[] args) {
